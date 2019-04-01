cc4adc91e69defcf5bbd8f62360a4753d4639283
massigualt <massimilia.gualtier3@studio.unibo.it>
2019-04-01 13:44:53 +0200

    Revert to sviluppo

--- src/distributedLogic/client/PlayersInterface.java
+++
@@ -1,9 +0,0 @@
-package distributedLogic.client;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface PlayersInterface extends Remote {
-
-    void receive(String message) throws RemoteException;
-}
